0cd998d6d547fa33a9e3150696d70ae7ab8316d5
Lolo-arch24 <ummusalama024@gmail.com>
2026-07-20 00:30:44 +0100

    Refactor snake game with movement and collision logic

--- main.py
+++ main.py
@@ -36,20 +36,84 @@ window.geometry(f"{window_width}x{window_height}+{window_x}+{window_y}")
 #initialize game
 snake = Tile(5*TILE_SIZE, 5*TILE_SIZE) #single tile, snake's head
 food = Tile(10*TILE_SIZE, 10*TILE_SIZE)
+snake_body = [] #multiple snake tiles
+velocityX = 0
+velocityY = 0
+game_over = False
+
+def change_direction(e): #e = event
+    #print(e)
+    #print(e.keysym)
+    global velocityX, velocityY, game_over
+    if (game_over):
+        return
+
+    if (e.keysym == "Up" and velocityY != 1):
+        velocityX = 0
+        velocityY = -1
+    elif (e.keysym == "Down" and velocityY != -1):
+        velocityX = 0
+        velocityY = 1
+    elif (e.keysym == "Left" and velocityX != 1):
+        velocityX = -1
+        velocityY = 0
+    elif (e.keysym == "Right" and velocityX != -1):
+        velocityX = 1
+        velocityY = 0
+
+def move():
+    global snake, food, snake_body, game_over
+    if (game_over):
+        return
+    
+    if (snake.x < 0 or snake.x >= WINDOW_WIDTH or snake.y < 0 or snake.y >= WINDOW_HEIGHT):
+        game_over = True
+        return
+    
+    for tile in snake_body:
+        if (snake.x == tile.x and snake.y == tile.y):
+            game_over = True
+            return
+
+    #collision
+    if (snake.x == food.x and snake.y == food.y):
+        snake_body.append(Tile(food.x, food.y))
+        food.x = random.randint(0, COLS-1) * TILE_SIZE
+        food.y = random.randint(0, ROWS-1) * TILE_SIZE
+
+    #update snake body
+    for i in range(len(snake_body)-1, -1, -1):
+        tile = snake_body[i]
+        if (i == 0):
+            tile.x = snake.x
+            tile.y = snake.y
+        else:
+            prev_tile = snake_body[i-1]
+            tile.x = prev_tile.x
+            tile.y = prev_tile.y
+
+    snake.x += velocityX * TILE_SIZE
+    snake.y += velocityY * TILE_SIZE
 
 def draw():
     global snake
+    move()
+
+    canvas.delete("all")
+
+     #draw food
+    canvas.create_rectangle(food.x, food.y, food.x + TILE_SIZE, food.y + TILE_SIZE, fill = "red")
 
     #draw snake
     canvas.create_rectangle(snake.x, snake.y, snake.x + TILE_SIZE, snake.y + TILE_SIZE, fill = "lime green")
 
-    #draw food
-    canvas.create_rectangle(food.x, food.y, food.x + TILE_SIZE, food.y + TILE_SIZE, fill = "red")
+    for tile in snake_body:
+        canvas.create_rectangle(tile.x, tile.y, tile.x + TILE_SIZE, tile.y + TILE_SIZE, fill = "lime green")
+
 
     window.after(100, draw) #100ms = 1/10 second, 10 frames/second
 
 draw()
 
-
-
+window.bind("<KeyRelease>", change_direction)
 window.mainloop()
